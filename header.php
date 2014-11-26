eb4287933b789da344301050e3a288eeb346cfcb
Becky Weirauch <wicktoweir@gmail.com>
2014-11-26 15:57:37 -0600

    Created php templates.

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../favicon.ico">
+      <title>Rebecca Weirauch</title>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+      <!-- Custom styles for this template -->
+      <link href="justified-nav.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+      <link href="jumbotron-narrow.css" rel="stylesheet">
+      <!-- Google Fonts -->
+      <link href='http://fonts.googleapis.com/css?family=Play:400,700' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+      <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+      <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+      <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+   </head>
+
+   <body>
+      <div class="container">
+         <div class="masthead">
+            <div role="navigation">
+               <ul class="nav nav-justified">
+                  <li ><a href="/"><img class="logo" src="img/logo.png"></a></li>
+                  <li class="nav-buttons"><a href="my-work.php">My Work</a></li>
+                  <li class="nav-buttons"><a href="contact.php">Contact Me</a></li>
+                  <li class="nav-buttons"><a href="/">Home</a></li>
+               </ul>
+            </div>
+         </div>
+
+
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php'; ?>
+
+         <!-- Jumbotron -->
+         <div class="jumbotron">
+            <h2 class="centered red-text">I'm a Web Designer and Developer.</h2>
+            <p class="lead">My name is Rebecca Weirauch. I started in the technology field in early 2012 while trying to find a new career path. After doing an introductory course in web design, I was hooked! Take a look at my portfolio to see the work I have done so far and to learn a bit more about me.</p>
+         </div>
+      </div> <!-- /container -->
+ 
+      <footer>
+         <div class="container">
+            <img class="img-responsive center-img" src="img/footer-img.jpg">
+         </div>
+      </footer>
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
